8f6cec1332ea4756aa1655c2f8a8fc0a66560835
Jorge F <jorge.m.flores@protonmail.com>
2021-12-29 06:08:03 +0000

    finished draft that validates solution is correct

---
+++ sudokuSolver/index.cpp
@@ -0,0 +1,16 @@
+#include "validate2.cpp"
+
+int main() {
+
+  cout << "\n" << "\n";
+  // cout << validateRows() << "\n";
+  // cout << validateColumns() << "\n";
+  checkAllCubes();
+  // for(auto& row: puzzle[1]){
+  //   for(auto num: row){
+  //     cout << num << endl;
+  //   };
+  // };
+
+  return 0;
+}
